ff6ddfc7d4800dfd6347100a2b6a036085975158
vcexnet <36216930+vcexnet@users.noreply.github.com>
2020-12-26 11:09:10 +0800

    Update e_wallet.js
    修复交易所行情和行情应用交易对显示
    
    2020.12.26
    1、修复首页行情交易所信息
    2、修复行情应用只给你价格显示下面交易对信息

--- js/page/e_wallet.js
+++ js/page/e_wallet.js
@@ -1,89 +1,48 @@
-
+// var userInfo = localStorage.getItem("user");
 var walletName = localStorage.getItem("walletName");
 var oneTime=localStorage.getItem("oneTime");
 var oldName;
 var file_content = [];
-var price;
+
+
+
 
 $(function() {
-    // Common.saveLocalData("walletName", "eVAr93C9FZQw4bMaXpU5hSK894WCwPdRBa");
-	showEAC();
-	//创建对应的文件
-     setTimeout(function(){createFileAll();}, 500);
-        
-	setTimeout(function(){
-		 FileUtil.readFile("EAC/nodeBook");
-		  }, 300);
-	if (walletName) {
 
-		showTransactions();
-		showDTransactions();
+	if (!walletName) {
+		$("#changeFile").css("display", "block");
+		showInputWalletName();
+	} else {
 		if(oneTime==0){
 			$("#walletName").val(walletName);
 			showInputWalletName();
 		}
 
-	} else {
-		$("#changeFile").css("display", "block");
-		showInputWalletName();
-
-
 	}
-	// document.getElementById('files').addEventListener('change', handleFileSelect, false);
-
-
+	document.getElementById('files').addEventListener('change', handleFileSelect, false);
+	showWallet();
+	showTransactions();
+	showDTransactions();
 });
-
-
-					// var data={"address":"address","newLable":"newLable","amount":"amount"};
-					// console.log(data.address);
-			function scaned(t, r, f) {
-				console.log("t:" + t + "r:" + r + "f:" + f);
-				var data= Common.codeResolver(r);
-							console.log(data);
-				window.location.href="send.html?data="+data;
-							
-			}
-
-
-			// 打开二维码扫描界面 
-			function openBarcode() {
-				createWithoutTitle('barcode_scan.html', {
-					titleNView: {
-						type: 'float',
-						backgroundColor: 'rgba(215,75,40,0.3)',
-						titleText: '扫一扫',
-						titleColor: '#FFFFFF',
-						autoBackButton: true,
-						buttons: [{
-							fontSrc: '_www/helloh5.ttf',
-							text: '\ue302',
-							fontSize: '18px',
-							onclick: 'javascript:scanPicture()'
-						}]
-					}
-				});
-			}
 //读取文件值
-// function handleFileSelect(evt) {
-// 	var file = evt.target.files[0]; // FileList object
-// 	var reader = new FileReader();
-// 	reader.onload = (function(theFile) {
-// 		return function(e) {
-// 			file_content.push(e.target.result);
-// 		};
-// 	})(file);
+function handleFileSelect(evt) {
+	var file = evt.target.files[0]; // FileList object
+	var reader = new FileReader();
+	reader.onload = (function(theFile) {
+		return function(e) {
+			file_content.push(e.target.result);
+		};
+	})(file);
 
-// 	reader.onloadend = function() {
-// 		var str = file_content[0];
-// 		$file_list = str.split(/\s+/);
+	reader.onloadend = function() {
+		var str = file_content[0];
+		$file_list = str.split(/\s+/);
 
-// 		oldName = $file_list;
-// 		console.log($file_list);
-// 		$("#walletName").val(oldName);
-// 	};
-// 	reader.readAsText(file);
-// }
+		oldName = $file_list;
+		console.log($file_list);
+	};
+	reader.readAsText(file);
+}
 //显示交易记录
 function showTransactions() {
 
@@ -96,7 +55,7 @@ function showTransactions() {
 		// 请求方式
 		type: "post",
 		contentType: "application/json",
-		url: ChangeEnv.path+walletName,
+		url: ChangeEnv.path,
 		username: ChangeEnv.username,
 		password: ChangeEnv.password,
 		dataType: "json",
@@ -107,49 +66,23 @@ function showTransactions() {
 		async: false,
 		data: JSON.stringify(datadata),
 		success: function(data) {
-			// console.log(data.result);
-
-			var dataList =data.result.sort(function(a,b){
-        return a.time < b.time ? 1 : -1
-    });;	
-			// var dataList=[];
-			// for(var i=0;i<dataLists.length;i++){
-			
-			// 	if(!dataLists[i].fee){
-			// 			// console.log("我看看");
-			// 		dataList.push(dataLists[i]);
-			// 	}
-			// }
-
-			var forTime=0;
+			console.log(data.result);
+			var dataList = data.result;
 			var htmls;
 			if (dataList.length != 0) {
-				if(dataList.length>3){
-					forTime=3
-				}else{
-					forTime=dataList.length
-				}
-				for (var i = 0; i < forTime; i++) {
-					htmls += '<tr style="border-top: solid #ccc 1px;" data-id="' + dataList[i].txid +'" id="showData">' +
-						'<td  style="color: #1EB032;font-size: 1.5rem;">√</td>'
-					if(dataList[i].amount>0){
-						htmls += '<td style="text-align: left;">收款自：' + dataList[i].address + '\n时间:' + Common.formatDate(dataList[i].time*1000) +
-							'</td>'
-						htmls += '<td style="color: #1EB032;">' + dataList[i].amount + '</td></tr>'
-						
-					}else{
-						htmls += '<td style="text-align: left;">付款到：' + dataList[i].address + '\n时间:' + Common.formatDate(dataList[i].time*1000) +
+				for (var i = 0; i < 2; i++) {
+					htmls += '<tr >' +
+						'<td data-id="' + dataList[i].txid +
+						'" style="color: #1EB032;font-size: 1.2rem;"><input type="button" class="show" value="√"/></td>'
+					htmls += '<td style="text-align: left;">收款自：' + dataList[i].address + '\n时间:' + Common.formatDate(dataList[i].time) +
 						'</td>'
-						htmls += '<td style="color: red;">' + dataList[i].amount + '</td></tr>'
-					}	
-					
+					htmls += '<td style="color: #1EB032;">' + dataList[i].amount + '</td></tr>'
 
 				};
 
 				$('#showDatas').append(htmls);
-				$("#showDatas").on("click", "#showData", function(event) {
-					console.log($(this))
-					var txId = $(this)[0].dataset.id;
+				$("#showDatas").on("click", ".show", function(event) {
+					var txId = $(this).parent().data("id");
 
 					showTransaction(txId);
 
@@ -178,7 +111,7 @@ function showTransaction(txId) {
 		// 请求方式
 		type: "post",
 		contentType: "application/json",
-		url: ChangeEnv.path+walletName,
+		url: ChangeEnv.path + walletName,
 		username: ChangeEnv.username,
 		password: ChangeEnv.password,
 		dataType: "json",
@@ -190,16 +123,12 @@ function showTransaction(txId) {
 		data: JSON.stringify(datadata),
 		success: function(data) {
 			var walletInfo = data.result;
-			var lable="";
 			$("#amount").val(walletInfo.amount + " Earthcoins");
-			$("#confirmations").val(walletInfo.confirmations);
-			$("#time").val(Common.formatDate(walletInfo.time*1000));
+			$("#confirmations").val(walletInfo.confirmations + " 个确认");
+			$("#time").val(Common.formatDate(walletInfo.time));
 			$("#txid").val(walletInfo.txid);
-			// $("#toaddress").val(walletInfo.amount);
-			if(walletInfo.details[0].label){
-				lable=walletInfo.details[0].label
-			}
-			$("#toaddress").val(lable + "  " + walletInfo.details[0].address);
+			// $("#toAdress").val(walletInfo.amount);
+			$("#toAdress").val(walletInfo.details[0].label + "  " + walletInfo.details[0].address);
 			$("#vout").val(walletInfo.details[0].vout);
 			showDetails();
 			// alert(data.result.walletversion);
@@ -224,7 +153,7 @@ function checkWallet(name) {
 		// 请求方式
 		type: "post",
 		contentType: "application/json",
-		url: ChangeEnv.path+name,
+		url: ChangeEnv.path + name,
 		username: ChangeEnv.username,
 		password: ChangeEnv.password,
 		dataType: "json",
@@ -237,11 +166,8 @@ function checkWallet(name) {
 		success: function(data) {
 			console.log(data);
 
-			if (data.result) {
-				Common.saveLocalData("walletName", name);
-				closeInputWalletName();
-			}
-			
+
+			Common.saveLocalData("walletName", walletName);
 
 
 		},
@@ -265,7 +191,7 @@ function showWallet() {
 		// 请求方式
 		type: "post",
 		contentType: "application/json",
-		url: ChangeEnv.path+walletName,
+		url: ChangeEnv.path+ walletName,
 		username: ChangeEnv.username,
 		password: ChangeEnv.password,
 		dataType: "json",
@@ -276,25 +202,18 @@ function showWallet() {
 		async: false,
 		data: JSON.stringify(datadata),
 		success: function(data) {
-			
+
 			if (data.result) {
 				var datas = data.result;
-				// $("#isBalance").val(datas.balance);
-				// $("#balance").val(datas.balance);
-				// $("#unconfirmed_balance").val(datas.unconfirmed_balance);
-				
-				/* 可使用余额 */
-			var a=document.getElementById("isBalance").value=datas.balance;
-				/* 待确认余额 */
-			var b=document.getElementById("unconfirmed_balance").value=datas.unconfirmed_balance;
-				/* 总额 */
-			var c=document.getElementById("balance").value=a + b;
-				/* 资产 */
-			var d=document.getElementById("assets").value="≈¥"+(c*price).toFixed(4);
-			// console.log(c+"+++++++++++"+price);
+
+				$("#isBalance").val(datas.balance);
+				$("#balance").val(datas.balance);
+				$("#unconfirmed_balance").val(datas.unconfirmed_balance);
 			}
+			// alert(data.result.walletversion);
+
+
 		},
-		
 		error: function(jqXHR) {
 			console.log("发生错误：" + jqXHR.status);
 		}
@@ -312,77 +231,38 @@ function showDTransactions() {
 			"coinname": "all"
 		},
 		success: function(datas) {
-			// console.log(datas);
+			console.log(datas.data);
 			var dataList = datas.data;
 			var htmls;
-			var arr=[];
-			var i=0;
-			if(dataList.length!=0){
-            for(let item in dataList ){
-				// console.log(item);
-				arr.push(dataList[item]);
-				++i;
-				if(i<=3){
-				htmls += "<tr style='border-top: solid #ccc 1px;'> <td style='color: #1EB032;font-size: 1rem;'>"+item.toUpperCase()+"</td>";
-				htmls += "<td style='text-align: center;'>"+item.toUpperCase()+"/CNC</td>";
-				htmls += "<td style='text-align: center;'>" + dataList[item].ticker.buy + "</td>";
-				if(dataList[item].ticker.range<0){
-					htmls += "<td style='color: red;'>" + ((dataList[item].ticker.range)*100).toFixed(2) + "%</td></tr>"
-					}else{
-						htmls += "<td style='color: #1EB032;'>" + ((dataList[item].ticker.range)*100).toFixed(2) + "%</td></tr>"
-					}
-				
-				}
-				}
-				}
-			// htmls += "<tr> <td style='color: #1EB032;font-size: 1.2rem;'>ae</td>"
-			// htmls += "<td style='text-align: center;'>AE/CNC</td>"
-			// htmls += "<td style='text-align: center;'>" + dataList.ae.ticker.buy + "</td>",
-			// htmls += "<td style='color: #1EB032;'>" + dataList.ae.ticker.range + "</td></tr>"
 
-			// htmls += "<tr> <td style='color: #1EB032;font-size: 1.2rem;'>btc</td>"
-			// htmls += "<td style='text-align: center;'>BTC/CNC</td>"
-			// htmls += "<td style='text-align: center;'>" + dataList.btc.ticker.buy + "</td>",
-			// 	htmls += "<td style='color: #1EB032;'>" + dataList.btc.ticker.range + "</td></tr>"
+			htmls += "<tr> <td style='color: #1EB032;font-size: 1.2rem;'>ae</td>"
+			htmls += "<td style='text-align: center;'>AE/CNC</td>"
+			htmls += "<td style='text-align: center;'>" + dataList.ae.buy + "</td>",
+				htmls += "<td style='color: #1EB032;'>" + dataList.ae.range + "</td></tr>"
+
+			htmls += "<tr> <td style='color: #1EB032;font-size: 1.2rem;'>btc</td>"
+			htmls += "<td style='text-align: center;'>BTC/CNC</td>"
+			htmls += "<td style='text-align: center;'>" + dataList.btc.buy + "</td>",
+				htmls += "<td style='color: #1EB032;'>" + dataList.btc.range + "</td></tr>"
 
 
 			$('#showExchanges').append(htmls);
 
-			// for (var i = 0; i < dataList.length - 1; i++) {
-			// 	console.log(dataList[i]);
+			for (var i = 0; i < dataList.length - 1; i++) {
+				console.log(dataList);
 
-			// }
+			}
+			// $.each(dataList, function(i, d) {
+			// 	console.log(d);
 
+
+			// });
 		},
 		dataType: 'json'
 	});
 
 }
-function showEAC() {
-	$.ajax({
-		url: "https://api.aex.zone/v3/trades.php",
-		method: "post",
-		data: {
-			"mk_type": "cnc",
-			"coinname": "eac"
-		},
-		success: function(datas) {
 
-			var dataList = datas.data;
-					// console.log(dataList[0].price);
-					price=dataList[0].price;
-						showWallet();
-			// $("#eacMoney").text(dataList[0].price);
-			
-			//var e=document.getElementById("eacNumebr").textContent=dataList[0].price;
-			
-			// var e=$("#eacNumebr").text(dataList[0].price);
-
-		},
-		dataType: 'json'
-	});
-
-}
 function showDetails() {
 	mui("#details").popover("toggle");
 }
@@ -431,51 +311,3 @@ function confirmName() {
 }
 
 
-//复制私钥
-function copyUrl() {
-	var walletName = $("#walletName").val();
-	if(walletName){
-		var Url = document.getElementById("walletName");
-		Url.select(); // 选择对象
-		document.execCommand("Copy"); //执行浏览器复制命令用户定义的代码区域用户定义的代码区域
-		mui.toast('复制成功');
-	}else{
-		mui.alert('请选择或者输入您的私钥', function() {
-		
-		
-		});
-	}
-	
-}
-// function texting(){
-
-// 		window.location.href="file.html";
-// 	    // FileUtil.showData();
-// }
-function createFileAll(){
-
-	var createFileAll= localStorage.getItem("createFileAll");
-	// FileUtil.showData();
-	// 	FileUtil.removeFile('EAC');
-		// Common.saveLocalData("createFileAll",1);
-	// alert(createFileAll+":createFileAll");
-	if(createFileAll!="isCreate"){
-		FileUtil.creatDirectory('EAC');
-
-		setTimeout(function(){
-			 FileUtil.creatFile('EAC/address');
-			 FileUtil.creatFile('EAC/paymentAddress');
-			 FileUtil.creatFile('EAC/nodeBook');
-			 FileUtil.creatFile('EAC/addressKey');
-			 Common.saveLocalData("createFileAll", "isCreate");
-			 
-			 // console.log("创建完毕");
-			 }, 500);
-		
-
-
-	}
-	// else{
-	// 	FileUtil.showData();
-	// 	}
-}
